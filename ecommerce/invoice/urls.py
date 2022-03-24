f13dc00de0cdd4ba1f8024134eea4ccf95a0b0ca
komalparmarp <komal.parmar@plutustec.com>
2022-03-24 11:17:25 +0530

    views

--- ecommerce/invoice/urls.py
+++ ecommerce/invoice/urls.py
@@ -4,8 +4,8 @@ from .views import *
 # from wkhtmltopdf.views import PDFTemplateView
 
 urlpatterns = [
-    # path('invoice/', GenerateInvoice.as_view(), name='generate_invoice'),
-    # path('invoice_download/', DownloadInvoice.as_view(), name="download_invoice"),
-    # path('share_invoice/', ShareInvoice.as_view(), name="share_invoice"),
-    path('wk/', MyPDF.as_view(template_name='pdf.html', filename='my_pdf.pdf'), name='pdf')
+    path('invoice/', GenerateInvoice.as_view(), name='generate_invoice'),
+    path('invoice_download/', DownloadInvoice.as_view(), name="download_invoice"),
+    path('share_invoice/', ShareInvoice.as_view(), name="share_invoice"),
+    # path('wk/', MyPDF.as_view(template_name='pdf.html', filename='my_pdf.pdf'), name='pdf')
 ]
--- ecommerce/invoice/views.py
+++ ecommerce/invoice/views.py
@@ -12,101 +12,100 @@ from django.core.mail import EmailMessage
 from django.conf import settings
 
 
-# def render_to_pdf(template_src, context_dict={}):
-#     template = get_template(template_src)
-#     html = template.render(context_dict)
-#     result = BytesIO()
-#     pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
-#     if not pdf.err:
-#         return result.getvalue()
-#     return None
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return result.getvalue()
+    return None
 
 
-#
-# class GenerateInvoice(APIView):
-#
-#     def post(self, request, *args, **kwargs):
-#         template = get_template('pdf.html')
-#         data = request.data
-#         order_id = data['order_id']
-#         order = Order.objects.get(id=order_id)
-#         pay = PaymentGateway.objects.get(order_id=order)
-#         user = order.user
-#         order_items = OrderItem.objects.filter(order=order)
-#         pdf = render_to_pdf('pdf.html', {'order_item': order_items, 'payment': pay, 'user': user, 'order': order})
-#         return HttpResponse(pdf, content_type='application/pdf')
-#
-#
-# class DownloadInvoice(APIView):
-#     def post(self, request):
-#         # template = get_template('pdf.html')
-#         data = request.data
-#         order_id = data['order_id']
-#         order = Order.objects.get(id=order_id)
-#         payment = PaymentGateway.objects.get(order_id=order)
-#         user = order.user
-#         invoice = Invoice(user=user, order_id=order_id, payment_method=payment.payment_type,
-#                           payment_status=payment.status, total_amount=payment.payment_price)
-#         invoice.save()
-#         order_items = OrderItem.objects.filter(order=order)
-#         for i in order_items:
-#             InvoiceItem.objects.create(invoice=invoice, product=i.product, product_amount=i.price)
-#
-#         pdf = render_to_pdf('pdf.html',
-#                             {'invoice': invoice, 'order_item': order_items, 'payment': payment, 'user': user,
-#                              'order': order})
-#
-#         if pdf:
-#             response = HttpResponse(pdf, content_type='application/pdf')
-#             filename = "Invoice_%s.pdf" % (data['order_id'])
-#             # content = "inline; filename = '%s'" % (filename)
-#             content = "attachment; filename = '%s'" % (filename)
-#             response['Content-Disposition'] = content
-#             return response
-#         return HttpResponse("not found")
-#
-#
-# class ShareInvoice(APIView):
-#     def post(self, request):
-#         template = get_template('pdf.html')
-#         data = request.data
-#         order_id = data['order_id']
-#         order = Order.objects.get(id=order_id)
-#         payment = PaymentGateway.objects.get(order_id=order)
-#         user = order.user
-#         invoice = Invoice(user=user, order_id=order_id, payment_method=payment.payment_type,
-#                           payment_status=payment.status, total_amount=payment.payment_price)
-#         invoice.save()
-#         order_items = OrderItem.objects.filter(order=order)
-#         for i in order_items:
-#             InvoiceItem.objects.create(invoice=invoice, product=i.product, product_amount=i.price)
-#
-#         pdf = render_to_pdf('pdf.html',
-#                             {'invoice': invoice, 'order_item': order_items, 'payment': payment, 'user': user,
-#                              'order': order})
-#
-#         if pdf:
-#             mail_subject = "Recent Order Details"
-#             email = EmailMessage(mail_subject, 'this is a message', settings.EMAIL_HOST_USER, [user.email])
-#             email.attach('new.pdf', pdf, "application/pdf")
-#             email.send()
-#         return Response({'msg': 'Invoice generated!'})
-#
-#
-from wkhtmltopdf.views import PDFTemplateView
+
+class GenerateInvoice(APIView):
+
+    def post(self, request, *args, **kwargs):
+        template = get_template('pdf.html')
+        data = request.data
+        order_id = data['order_id']
+        order = Order.objects.get(id=order_id)
+        pay = PaymentGateway.objects.get(order_id=order)
+        user = order.user
+        order_items = OrderItem.objects.filter(order=order)
+        pdf = render_to_pdf('pdf.html', {'order_item': order_items, 'payment': pay, 'user': user, 'order': order})
+        return HttpResponse(pdf, content_type='application/pdf')
 
 
+class DownloadInvoice(APIView):
+    def post(self, request):
+        # template = get_template('pdf.html')
+        data = request.data
+        order_id = data['order_id']
+        order = Order.objects.get(id=order_id)
+        payment = PaymentGateway.objects.get(order_id=order)
+        user = order.user
+        invoice = Invoice(user=user, order_id=order_id, payment_method=payment.payment_type,
+                          payment_status=payment.status, total_amount=payment.payment_price)
+        invoice.save()
+        order_items = OrderItem.objects.filter(order=order)
+        for i in order_items:
+            InvoiceItem.objects.create(invoice=invoice, product=i.product, product_amount=i.price)
+
+        pdf = render_to_pdf('pdf.html',
+                            {'invoice': invoice, 'order_item': order_items, 'payment': payment, 'user': user,
+                             'order': order})
+
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf')
+            filename = "Invoice_%s.pdf" % (data['order_id'])
+            # content = "inline; filename = '%s'" % (filename)
+            content = "attachment; filename = '%s'" % (filename)
+            response['Content-Disposition'] = content
+            return response
+        return HttpResponse("not found")
+
+
+class ShareInvoice(APIView):
+    def post(self, request):
+        template = get_template('pdf.html')
+        data = request.data
+        order_id = data['order_id']
+        order = Order.objects.get(id=order_id)
+        payment = PaymentGateway.objects.get(order_id=order)
+        user = order.user
+        invoice = Invoice(user=user, order_id=order_id, payment_method=payment.payment_type,
+                          payment_status=payment.status, total_amount=payment.payment_price)
+        invoice.save()
+        order_items = OrderItem.objects.filter(order=order)
+        for i in order_items:
+            InvoiceItem.objects.create(invoice=invoice, product=i.product, product_amount=i.price)
+
+        pdf = render_to_pdf('pdf.html',
+                            {'invoice': invoice, 'order_item': order_items, 'payment': payment, 'user': user,
+                             'order': order})
+
+        if pdf:
+            mail_subject = "Recent Order Details"
+            email = EmailMessage(mail_subject, 'this is a message', settings.EMAIL_HOST_USER, [user.email])
+            email.attach('new.pdf', pdf, "application/pdf")
+            email.send()
+        return Response({'msg': 'Invoice generated!'})
+
 #
-
-class MyPDF(PDFTemplateView):
-
-    order = Order.objects.get(id=order_id)
-    pay = PaymentGateway.objects.get(order_id=order)
-    user = order.user
-    order_items = OrderItem.objects.filter(order=order)
-   
-    # cmd_options = {
-    #     'margin-top': 3,
-    # # }
-    # extra_context = {'users': Invoice.objects.all(), 'payment': PaymentGateway.objects.all(),
-    #                  'order': Order.objects.all(), 'item': OrderItem.objects.all()}
+# from wkhtmltopdf.views import PDFTemplateView
+#
+#
+# class MyPDF(PDFTemplateView):
+#
+#
+#     order = Order.objects.get(id=order_id)
+#     pay = PaymentGateway.objects.get(order_id=order)
+#     user = order.user
+#     order_items = OrderItem.objects.filter(order=order)
+#
+#     # cmd_options = {
+#     #     'margin-top': 3,
+#     # # }
+#     # extra_context = {'users': Invoice.objects.all(), 'payment': PaymentGateway.objects.all(),
+#     #                  'order': Order.objects.all(), 'item': OrderItem.objects.all()}
